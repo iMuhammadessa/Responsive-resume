63c4015c4a608f1eac1bb37314b15594a494676b
Muhammad Essa <iMuhammadessa@gmail.com>
2021-06-17 16:19:35 +0500

    Documented

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,29 @@
-/*==================== SHOW MENU ====================*/
+/* SHOW MENU */
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
     
-    // Validate that variables exist
+    /* Validate that variables exist */
     if(toggle && nav){
         toggle.addEventListener('click', ()=>{
-            // We add the show-menu class to the div tag with the nav__menu class
+            /* We add the show-menu class to the div tag with the nav__menu class */
             nav.classList.toggle('show-menu')
         })
     }
 }
 showMenu('nav-toggle','nav-menu')
 
-/*==================== REMOVE MENU MOBILE ====================*/
+/* REMOVE MENU MOBILE */
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
+    /* When we click on each nav__link, we remove the show-menu class */
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+/* SCROLL SECTIONS ACTIVE LINK */
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
@@ -43,61 +43,61 @@ function scrollActive(){
 }
 window.addEventListener('scroll', scrollActive)
 
-/*==================== SHOW SCROLL TOP ====================*/ 
+/* SHOW SCROLL TOP */ 
 function scrollTop(){
     const scrollTop = document.getElementById('scroll-top');
-    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    /* When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class */
     if(this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop)
 
-/*==================== DARK LIGHT THEME ====================*/ 
+/* DARK LIGHT THEME */ 
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'bx-sun'
 
-// Previously selected topic (if user selected)
+/* Previously selected topic (if user selected) */
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
-// We obtain the current theme that the interface has by validating the dark-theme class
+/* We obtain the current theme that the interface has by validating the dark-theme class */
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
 
-// We validate if the user previously chose a topic
+/* We validate if the user previously chose a topic */
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  /* If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark */
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
   themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
 }
 
-// Activate / deactivate the theme manually with the button
+/* Activate / deactivate the theme manually with the button */
 themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
+    /* Add or remove the dark / icon theme */
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
+    /* We save the theme and the current icon that the user chose */
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
-/*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/ 
+/* REDUCE THE SIZE AND PRINT ON AN A4 SHEET */ 
 function scaleCv(){
     document.body.classList.add('scale-cv')
 }
 
-/*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/ 
+/* REMOVE THE SIZE WHEN THE CV IS DOWNLOADED */ 
 function removeScale(){
     document.body.classList.remove('scale-cv')
 }
 
-/*==================== GENERATE PDF ====================*/ 
-// PDF generated area
+/* GENERATE PDF */ 
+/* PDF generated area */
 let areaCv = document.getElementById('area-cv')
 
 let resumeButton = document.getElementById('resume-button')
 
-// Html2pdf options
+/* Html2pdf options */
 let opt = {
   margin:       0,
   filename:     'myResume.pdf',
@@ -106,19 +106,20 @@ let opt = {
   jsPDF:        { format: 'a4', orientation: 'portrait' }
 };
 
-// Function to call areaCv and Html2Pdf options 
+/* Function to call areaCv and Html2Pdf options */
     function generateResume(){
     html2pdf(areaCv, opt)
 }
 
-// When the button is clicked, it executes the three functions
+/* When the button is clicked, it executes the three functions */
     resumeButton.addEventListener('click', () =>{
-    // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
+
+    /* 1. The class .scale-cv is added to the body, where it reduces the size of the elements */
     scaleCv()
 
-    // 2. The PDF is generated
+    /* 2. The PDF is generated */
     generateResume()
 
-    // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+    /* 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size. */
         setTimeout(removeScale, 5000)
 })
